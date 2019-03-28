a8cb8c024551a33dc2e178aeca25efd975e842f8
Paul Melnikow <email@paulmelnikow.com>
2019-03-28 14:51:43 -0400

    Check lint in CI

--- dev.py
+++ dev.py
@@ -8,10 +8,10 @@ from executor import execute
 def python_source_files():
     import glob
 
-    include_paths = glob.glob("*.py") + glob.glob("polliwog/*.py") + ["doc/"]
+    include_paths = glob.glob("*.py") + glob.glob("polliwog/*.py") #+ ["doc/"]
 
     # TODO: Use flake8; argh.
-    exclude_paths = []
+    exclude_paths = ['polliwog/__init__.py']
 
     return [x for x in include_paths if x not in exclude_paths]
 
