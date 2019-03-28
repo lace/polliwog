a5ef41e062289c2d4901e9036554007f7bb6c0f7
Paul Melnikow <email@paulmelnikow.com>
2019-03-28 13:03:05 -0400

    Add dev script

---
+++ dev.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+import os
+import click
+from executor import execute
+
+
+def python_source_files():
+    import glob
+
+    include_paths = glob.glob("*.py") + glob.glob("polliwog/*.py") + ["doc/"]
+
+    # TODO: Use flake8; argh.
+    exclude_paths = []
+
+    return [x for x in include_paths if x not in exclude_paths]
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def init():
+    execute("pip2 install --upgrade -r requirements_dev_py2.txt")
+    execute("pip3 install --upgrade -r requirements_dev_py3.txt")
+
+
+@cli.command()
+def test():
+    # execute("pytest")
+    execute("nose2-2.7")
+
+
+@cli.command()
+def coverage():
+    # execute("pytest --cov=vg")
+    pass
+
+
+@cli.command()
+def coverage_report():
+    execute("coverage html")
+    execute("open htmlcov/index.html")
+
+
+# @cli.command()
+# def test_both():
+#     execute("python2 -m pytest")
+#     execute("python3 -m pytest")
+
+
+@cli.command()
+def lint():
+    execute("pyflakes", *python_source_files())
+
+
+@cli.command()
+def black():
+    execute("black", *python_source_files())
+
+
+@cli.command()
+def black_check():
+    execute("black", "--check", *python_source_files())
+
+
+@cli.command()
+def doc():
+    execute("rm -rf build/ doc/build/ doc/api/")
+    execute("sphinx-build -b singlehtml doc doc/build")
+
+
+@cli.command()
+def doc_open():
+    execute("open doc/build/index.html")
+
+
+@cli.command()
+def publish():
+    execute("rm -rf dist/")
+    execute("python setup.py sdist")
+    execute("twine upload dist/*")
+
+
+if __name__ == "__main__":
+    os.chdir(os.path.abspath(os.path.dirname(__file__)))
+    cli()
