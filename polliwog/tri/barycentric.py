9bac124eda34b1c78e371b63aff0728edad6c25f
Paul Melnikow <github@paulmelnikow.com>
2019-04-03 15:15:08 -0700

    Rewrite `polliwog.tri.barycentric` with new function signature + test (#14)

--- polliwog/tri/barycentric.py
+++ polliwog/tri/barycentric.py
@@ -1,27 +1,42 @@
 import numpy as np
+import vg
 
 
-def barycentric_coordinates_of_projection(p, q, u, v):
-    """ Given a point, gives projected coords of that point to a triangle
-    in barycentric coordinates.
+def compute_barycentric_coordinates(vertices_of_tris, points):
+    """
+    Compute barycentric coordinates for the projection of a set of points to a
+    given set of triangles specfied by their vertices.
 
-    See Heidrich, Computing the Barycentric Coordinates of a Projected Point, JGT 05
-    at http://www.cs.ubc.ca/~heidrich/Papers/JGT.05.pdf
+    These barycentric coordinates can refer to points outside the triangle.
+    This happens when one of the coordinates is negative. However they can't
+    specify points outside the triangle's plane. (That requires tetrahedral
+    coordinates.)
+
+    The returned coordinates supply a linear combination which, applied to the
+    vertices, returns the projection of the original point the plane of the
+    triangle.
 
     Args:
-        p: point to project
-        q: a vertex of the triangle to project into
-        u,v: edges of the the triangle such that it has vertices q, q+u, q+v
+        vertices_of_tris (np.arraylike): A set of triangle vertices as `kx3x3`.
+        points (np.arraylike): Coordinates of points as `kx3`.
 
     Returns:
-        b: barycentric coordinates of p's projection in triangle defined by q,u,v
-            vectorized so p,q,u,v can all be 3xN
-    """
+        np.ndarray: Barycentric coordinates as `kx3`
 
-    p = p.T
-    q = q.T
-    u = u.T
-    v = v.T
+    See Also:
+        https://en.wikipedia.org/wiki/Barycentric_coordinate_system
+
+    See Also:
+        Heidrich, Computing the Barycentric Coordinates of a Projected Point, JGT 05
+        at http://www.cs.ubc.ca/~heidrich/Papers/JGT.05.pdf
+    """
+    k = vg.shape.check(locals(), "vertices_of_tris", (-1, 3, 3))
+    vg.shape.check(locals(), "points", (k, 3))
+
+    p = points.T
+    q = vertices_of_tris[:, 0].T
+    u = (vertices_of_tris[:, 1] - vertices_of_tris[:, 0]).T
+    v = (vertices_of_tris[:, 2] - vertices_of_tris[:, 0]).T
 
     n = np.cross(u, v, axis=0)
     s = np.sum(n * n, axis=0)
