506a3788e2ff9d26499ca9176646866ee30ca3bb
Paul Melnikow <email@paulmelnikow.com>
2019-10-05 08:42:20 -0400

    Improve tests

--- polliwog/plane/test_equations.py
+++ polliwog/plane/test_equations.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 import vg
-from . import equations
+from .equations import plane_equation_from_points, plane_normal_from_points
 
 
 def assert_plane_equation_satisfies_points(plane_equation, points):
@@ -12,20 +12,43 @@ def assert_plane_equation_satisfies_points(plane_equation, points):
 
 def test_plane_equation_from_points():
     points = np.array([[1, 1, 1], [-1, 1, 0], [2, 0, 3]])
-    equation = equations.plane_equation_from_points(points)
+    equation = plane_equation_from_points(points)
     assert_plane_equation_satisfies_points(equation, points)
 
 
 def test_plane_equation_from_points_is_in_expected_orientation():
-    # Set up.
-    points = np.array([vg.basis.x, vg.basis.y, vg.basis.neg_x])
-
-    # Act.
-    equation = equations.plane_equation_from_points(points)
-
-    # Confidence check.
-    assert_plane_equation_satisfies_points(equation, points)
-
-    # Assert.
+    equation = plane_equation_from_points(
+        np.array([vg.basis.x, vg.basis.y, vg.basis.neg_x])
+    )
     normal = equation[:3]
     np.testing.assert_array_equal(normal, vg.basis.z)
+
+
+def test_plane_equation_from_points_stacked():
+    points = np.array(
+        [[[1, 1, 1], [-1, 1, 0], [2, 0, 3]], [vg.basis.x, vg.basis.y, vg.basis.neg_x]]
+    )
+    equations = plane_equation_from_points(points)
+    for these_points, this_equation in zip(points, equations):
+        assert_plane_equation_satisfies_points(this_equation, these_points)
+
+
+def test_plane_normal_from_points():
+    np.testing.assert_array_almost_equal(
+        plane_normal_from_points(np.array([vg.basis.x, vg.basis.y, vg.basis.neg_x])),
+        vg.basis.z,
+    )
+
+
+def test_plane_normal_from_points_stacked():
+    np.testing.assert_array_almost_equal(
+        plane_normal_from_points(
+            np.array(
+                [
+                    [vg.basis.x, vg.basis.y, vg.basis.z],
+                    [vg.basis.x, vg.basis.y, vg.basis.neg_x],
+                ]
+            )
+        ),
+        np.array([np.repeat(math.sqrt(1.0 / 3.0), 3), vg.basis.z]),
+    )
