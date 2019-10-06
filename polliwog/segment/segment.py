47ea3eb5016d871298354ec71269abf6da6abb22
Paul Melnikow <github@paulmelnikow.com>
2019-10-06 18:23:07 -0400

    Add closest_point_of_line_segment function (#69)

--- polliwog/segment/segment.py
+++ polliwog/segment/segment.py
@@ -1,4 +1,5 @@
 import numpy as np
+import vg
 
 
 def partition(v, partition_size=5):
@@ -100,3 +101,36 @@ def partition_segment_old(p1, p2, partition_size=5):
     domain = partition_width * np.arange(1, partition_size)
 
     return p1 + unit_direction * domain[:, np.newaxis]
+
+
+def closest_point_of_line_segment(points, start_points, segment_vectors):
+    # Adapted from public domain algorithm
+    # https://gdbooks.gitbooks.io/3dcollisions/content/Chapter1/closest_point_on_line.html
+    k = vg.shape.check(locals(), "points", (-1, 3))
+    vg.shape.check(locals(), "start_points", (k, 3))
+    vg.shape.check(locals(), "segment_vectors", (k, 3))
+
+    # Compute t values such that
+    # `result = reference_point + t * vector_along_line`.
+    square_of_segment_lengths = vg.dot(segment_vectors, segment_vectors)
+    # Degenerate segments will cause a division by zero, so handle that.
+    t = np.nan_to_num(
+        vg.dot(points - start_points, segment_vectors) / square_of_segment_lengths
+    )
+
+    # When `0 <= t <= 1`, the point is on the segment. When `t < 0`, the
+    # closest point is the segment start. When `t > 1`, the closest point is
+    # the segment end.
+    #
+    # Start with the `0 <= t <= 1 case`, then use masks to apply the clamp.
+    result = start_points + t.reshape(-1, 1) * segment_vectors
+
+    clamped_to_start_point = t < 0
+    result[clamped_to_start_point] = start_points[clamped_to_start_point]
+
+    clamped_to_end_point = t > 1
+    result[clamped_to_end_point] = (
+        start_points[clamped_to_end_point] + segment_vectors[clamped_to_end_point]
+    )
+
+    return result
--- polliwog/segment/test_segment.py
+++ polliwog/segment/test_segment.py
@@ -1,6 +1,11 @@
 import pytest
 import numpy as np
-from .segment import partition, partition_segment, partition_segment_old
+from .segment import (
+    partition,
+    partition_segment,
+    partition_segment_old,
+    closest_point_of_line_segment,
+)
 
 
 def test_partition_segment_old_raises_exception_for_invalid_partition_size_type():
@@ -239,3 +244,43 @@ def test_partition_adds_points_for_nonequal_arbitrarily_oriented_line():
     )
 
     np.testing.assert_array_almost_equal(partition(v), expected)
+
+
+def test_closest_point_of_line_segment():
+    # Adapted from public domain algorithm
+    # https://gdbooks.gitbooks.io/3dcollisions/content/Chapter1/closest_point_on_line.html
+    p1 = np.array([-3.0, -2.0, -1.0])
+    p2 = np.array([1.0, 2.0, 3.0])
+    segment_vector = p2 - p1
+
+    query_points = np.array(
+        [
+            [7, -2, -1],
+            [1, 5, -5],
+            [-7, -10, -4],
+            [-4, -7, -8],
+            [-6, 5, 7],
+            [1, 6, 8],
+            [7, 8, 5],
+            [5, 5, 3],
+        ]
+    )
+
+    expected_closest_points = np.array(
+        [
+            p1 + 10.0 / 12.0 * segment_vector,
+            p1 + 7.0 / 12.0 * segment_vector,
+            p1,
+            p1,
+            p2,
+            p2,
+            p2,
+            p2,
+        ]
+    )
+    closest_points = closest_point_of_line_segment(
+        points=query_points,
+        start_points=np.broadcast_to(p1, query_points.shape),
+        segment_vectors=np.broadcast_to(segment_vector, query_points.shape),
+    )
+    np.testing.assert_array_almost_equal(closest_points, expected_closest_points)
