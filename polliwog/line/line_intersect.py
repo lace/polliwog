2a3bb43ab006534f743f2504bdbb56b12d2a2829
Paul Melnikow <email@paulmelnikow.com>
2019-10-19 14:59:27 +0200

    line_intersect2: Rename, return None instead of nan’s, and 100% coverage

--- polliwog/line/line_intersect.py
+++ polliwog/line/line_intersect.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def line_intersect(p0, q0, p1, q1):
+def line_intersect2(p0, q0, p1, q1):
     """
     Intersect two lines: (p0, q0) and (p1, q1). Each should be a 2D
     point.
@@ -20,13 +20,12 @@ def line_intersect(p0, q0, p1, q1):
     rhs1 = p1[1] * dx - dy * p1[0]
 
     a = np.array([lhs0, lhs1])
-
     b = np.array([rhs0, rhs1])
 
     try:
         return np.linalg.solve(a, b)
     except np.linalg.LinAlgError:
-        return np.array([np.nan, np.nan])
+        return None
 
 
 def line_intersect3(p0, q0, p1, q1):
@@ -37,6 +36,7 @@ def line_intersect3(p0, q0, p1, q1):
     """
     e = p0 - q0  # direction of line 0
     f = p1 - q1  # direction of line 1
+
     # Check for special case where we're given the intersection
     # Note that we must check for these, because if p0 == p1 then
     # g would be zero length and we can't continue
@@ -44,14 +44,16 @@ def line_intersect3(p0, q0, p1, q1):
         return p0
     if np.all(q0 == p1) or np.all(p0 == q1):
         return q0
+
     g = p0 - p1  # line between to complete a triangle
     h = np.cross(f, g)
     k = np.cross(f, e)
     h_ = np.linalg.norm(h)
     k_ = np.linalg.norm(k)
     if h_ == 0 or k_ == 0:
-        # there is no intesection; either parallel (k=0) or colinear (both=0) lines
+        # There is no intesection; either parallel (k=0) or collinear (both=0) lines.
         return None
+
     l = h_ / k_ * e
     sign = -1 if np.all(h / h_ == k / k_) else +1
     return p0 + sign * l
