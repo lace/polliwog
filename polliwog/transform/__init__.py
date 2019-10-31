06950e412adef614913d8e42d908f5a2d2f6faf8
Paul Melnikow <github@paulmelnikow.com>
2019-10-31 11:11:32 -0400

    Remove `find_rigid_rotation()` (#91)
    Ref #77
    
    This seems like a not-very-important piece of functionality and has no tests, so it's being removed for now. It's easy to find in the history, and if it's needed in the future, can be re-added and brought up to spec.

--- polliwog/transform/__init__.py
+++ polliwog/transform/__init__.py
@@ -1,5 +1,4 @@
 from .composite import CompositeTransform
 from .coordinate_manager import CoordinateManager
 from .rigid_transform import find_rigid_transform
-from .rigid_transform import find_rigid_rotation
 from .rotation import rotation_from_up_and_look
