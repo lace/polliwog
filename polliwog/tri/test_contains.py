7f71a67c1595ea608baaf3f86df2ec9b3e3e147c
Paul Melnikow <email@paulmelnikow.com>
2019-11-05 23:26:07 -0500

    Lint using flake8

--- polliwog/tri/test_contains.py
+++ polliwog/tri/test_contains.py
@@ -7,14 +7,14 @@ def test_contains_coplanar_point():
     b = np.array([4.0, 0.1, 0.0])
     c = np.array([3.0, 3.1, 0.0])
 
-    assert contains_coplanar_point(a, b, c, a) == True
-    assert contains_coplanar_point(a, b, c, b) == True
-    assert contains_coplanar_point(a, b, c, c) == True
-    assert contains_coplanar_point(a, b, c, np.array([2.0, 1.0, 0.0])) == True
+    assert contains_coplanar_point(a, b, c, a) is True
+    assert contains_coplanar_point(a, b, c, b) is True
+    assert contains_coplanar_point(a, b, c, c) is True
+    assert contains_coplanar_point(a, b, c, np.array([2.0, 1.0, 0.0])) is True
 
     # Unexpected, as it's not in the plane, though if projected to the plane,
     # it is in the triangle.
-    assert contains_coplanar_point(a, b, c, np.array([0.0, 0.0, 1.0])) == True
+    assert contains_coplanar_point(a, b, c, np.array([0.0, 0.0, 1.0])) is True
 
-    assert contains_coplanar_point(a, b, c, np.array([2.0, 0.0, 0.0])) == False
-    assert contains_coplanar_point(a, b, c, np.array([2.0, 5.0, 0.0])) == False
+    assert contains_coplanar_point(a, b, c, np.array([2.0, 0.0, 0.0])) is False
+    assert contains_coplanar_point(a, b, c, np.array([2.0, 5.0, 0.0])) is False
