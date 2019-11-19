50d70f96e5f46ee0c8d793161f00ecf8ffbe4790
Paul Melnikow <github@paulmelnikow.com>
2019-11-19 15:21:21 -0500

    `rotation_from_up_and_look()`: Require input to be a `np.array` (#112)

--- polliwog/transform/test_rotation.py
+++ polliwog/transform/test_rotation.py
@@ -1,36 +1,41 @@
 import numpy as np
 import pytest
+import vg
 from .rotation import euler, rotation_from_up_and_look
+
+origin = np.zeros(3)
 
 
 def test_starting_with_canonical_reference_frame_gives_identity():
-    result = rotation_from_up_and_look(up=[0, 1, 0], look=[0, 0, 1])
+    result = rotation_from_up_and_look(up=vg.basis.y, look=vg.basis.z)
     np.testing.assert_array_almost_equal(result, np.eye(3))
 
 
 def test_raises_value_error_with_zero_length_inputs():
     with pytest.raises(ValueError):
-        rotation_from_up_and_look(up=[0, 0, 0], look=[0, 0, 1])
+        rotation_from_up_and_look(up=origin, look=vg.basis.z)
     with pytest.raises(ValueError):
-        rotation_from_up_and_look(up=[0, 1, 0], look=[0, 0, 0])
+        rotation_from_up_and_look(up=vg.basis.y, look=origin)
 
 
 def test_raises_value_error_with_collinear_inputs():
     with pytest.raises(ValueError):
-        rotation_from_up_and_look(up=[0, 0, 1], look=[0, 0, 1])
+        rotation_from_up_and_look(up=vg.basis.z, look=vg.basis.z)
     with pytest.raises(ValueError):
-        rotation_from_up_and_look(up=[0, 0, 1], look=[0, 0, -1])
+        rotation_from_up_and_look(up=vg.basis.z, look=vg.basis.neg_z)
 
 
 def test_normalizes_inputs():
-    result = rotation_from_up_and_look(up=[0, 42, 0], look=[0, 0, 13])
+    result = rotation_from_up_and_look(
+        up=np.array([0, 42, 0]), look=np.array([0, 0, 13])
+    )
     np.testing.assert_array_almost_equal(result, np.eye(3))
 
 
 def test_always_outputs_float64():
     result = rotation_from_up_and_look(
-        up=np.array([0, 1, 0], dtype=np.float32),
-        look=np.array([0, 0, 1], dtype=np.float32),
+        up=np.array(vg.basis.y, dtype=np.float32),
+        look=np.array(vg.basis.z, dtype=np.float32),
     )
     assert result.dtype == np.float64
     np.testing.assert_array_almost_equal(result, np.eye(3))
@@ -38,49 +43,59 @@ def test_always_outputs_float64():
 
 def test_vary_look_alone():
     np.testing.assert_array_almost_equal(
-        euler([0, 45]), rotation_from_up_and_look(up=[0, 1, 0], look=[-1, 0, 1])
+        euler([0, 45]),
+        rotation_from_up_and_look(up=vg.basis.y, look=np.array([-1, 0, 1])),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 90]), rotation_from_up_and_look(up=[0, 1, 0], look=[-1, 0, 0])
+        euler([0, 90]), rotation_from_up_and_look(up=vg.basis.y, look=vg.basis.neg_x)
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 135]), rotation_from_up_and_look(up=[0, 1, 0], look=[-1, 0, -1])
+        euler([0, 135]),
+        rotation_from_up_and_look(up=vg.basis.y, look=np.array([-1, 0, -1])),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 180]), rotation_from_up_and_look(up=[0, 1, 0], look=[0, 0, -1])
+        euler([0, 180]), rotation_from_up_and_look(up=vg.basis.y, look=vg.basis.neg_z)
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 225]), rotation_from_up_and_look(up=[0, 1, 0], look=[1, 0, -1])
+        euler([0, 225]),
+        rotation_from_up_and_look(up=vg.basis.y, look=np.array([1, 0, -1])),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 270]), rotation_from_up_and_look(up=[0, 1, 0], look=[1, 0, 0])
+        euler([0, 270]), rotation_from_up_and_look(up=vg.basis.y, look=vg.basis.x)
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 315]), rotation_from_up_and_look(up=[0, 1, 0], look=[1, 0, 1])
+        euler([0, 315]),
+        rotation_from_up_and_look(up=vg.basis.y, look=np.array([1, 0, 1])),
     )
 
 
 def test_vary_up_alone():
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 45]), rotation_from_up_and_look(up=[1, 1, 0], look=[0, 0, 1])
+        euler([0, 0, 45]),
+        rotation_from_up_and_look(up=np.array([1, 1, 0]), look=vg.basis.z),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 90]), rotation_from_up_and_look(up=[1, 0, 0], look=[0, 0, 1])
+        euler([0, 0, 90]), rotation_from_up_and_look(up=vg.basis.x, look=vg.basis.z)
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 135]), rotation_from_up_and_look(up=[1, -1, 0], look=[0, 0, 1])
+        euler([0, 0, 135]),
+        rotation_from_up_and_look(up=np.array([1, -1, 0]), look=vg.basis.z),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 180]), rotation_from_up_and_look(up=[0, -1, 0], look=[0, 0, 1])
+        euler([0, 0, 180]),
+        rotation_from_up_and_look(up=vg.basis.neg_y, look=vg.basis.z),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 225]), rotation_from_up_and_look(up=[-1, -1, 0], look=[0, 0, 1])
+        euler([0, 0, 225]),
+        rotation_from_up_and_look(up=np.array([-1, -1, 0]), look=vg.basis.z),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 270]), rotation_from_up_and_look(up=[-1, 0, 0], look=[0, 0, 1])
+        euler([0, 0, 270]),
+        rotation_from_up_and_look(up=vg.basis.neg_x, look=vg.basis.z),
     )
     np.testing.assert_array_almost_equal(
-        euler([0, 0, 315]), rotation_from_up_and_look(up=[-1, 1, 0], look=[0, 0, 1])
+        euler([0, 0, 315]),
+        rotation_from_up_and_look(up=np.array([-1, 1, 0]), look=vg.basis.z),
     )
 
 
