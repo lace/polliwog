cb3032aa3dd4b0e29216aef8c4cca4b3c8724755
Paul Melnikow <email@paulmelnikow.com>
2019-11-25 11:53:25 -0500

    Reformat

--- polliwog/polyline/test_polyline.py
+++ polliwog/polyline/test_polyline.py
@@ -825,10 +825,15 @@ def test_sliced_at_indices():
         Polyline(v=example_vs, is_closed=True).sliced_at_indices(3, 3).v,
         np.array([[1.0, 2.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0]]),
     )
-    with pytest.raises(ValueError, match=r"For an open polyline, start index of slice should be less than stop index"):
+    with pytest.raises(
+        ValueError,
+        match=r"For an open polyline, start index of slice should be less than stop index",
+    ):
         np.testing.assert_array_almost_equal(
             Polyline(v=example_vs, is_closed=False).sliced_at_indices(3, 3).v,
-            np.array([[1.0, 2.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0]]),
+            np.array(
+                [[1.0, 2.0, 0.0], [0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [1.0, 1.0, 0.0]]
+            ),
         )
 
 
