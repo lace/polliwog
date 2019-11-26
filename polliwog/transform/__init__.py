bb1f402c3183479489e6be1618f7aaf3e3fc1be8
Paul Melnikow <email@paulmelnikow.com>
2019-11-25 19:25:07 -0500

    Fix

--- polliwog/transform/__init__.py
+++ polliwog/transform/__init__.py
@@ -1,5 +1,4 @@
 from .composite import CompositeTransform  # noqa: F401
 from .coordinate_manager import CoordinateManager  # noqa: F401
 from .rigid_transform import find_rigid_rotation, find_rigid_transform  # noqa: F401
-from .rigid_transform import find_rigid_transform  # noqa: F401
 from .rotation import rotation_from_up_and_look  # noqa: F401
