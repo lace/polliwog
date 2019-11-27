225eda15344ba97f1ee19ae4a9c3040f991cb939
Alexander Weiss <aweiss@amazon.com>
2019-11-27 14:15:07 -0500

    Add more tests

--- polliwog/transform/test_rigid_transform.py
+++ polliwog/transform/test_rigid_transform.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from .rigid_transform import find_rigid_rotation, find_rigid_transform
 from .rotation import euler
@@ -46,9 +47,54 @@ def test_rigid_transform_from_rotation_translation_and_scale():
     np.testing.assert_almost_equal(s, expected_scale)
 
 
+def test_rigid_transform_with_reflection():
+    a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v.T
+    b = a * -1
+    with pytest.raises(ValueError):
+        find_rigid_transform(a, b)
+
+
+def test_rigid_transform_with_reflection_but_solve_anyway():
+    a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v.T
+    b = a * -1
+    expected_R = np.eye(3)
+    expected_R[0][0] = expected_R[1][1] = -1
+    expected_t = np.array([[0.0, 0.0, -1.0]]).T
+    R, t = find_rigid_transform(a, b, fail_in_degenerate_cases=False)
+    np.testing.assert_array_almost_equal(R, expected_R)
+    np.testing.assert_array_almost_equal(t, expected_t)
+
+
 def test_rigid_rotation():
     a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v.T
     expected_R = euler([30, 15, 21])
     b = expected_R.dot(a)
     R = find_rigid_rotation(a, b)
+    np.testing.assert_array_almost_equal(R, expected_R)
+
+
+def test_rigid_rotation_single_point():
+    a = np.array([[1, 2, 3]]).T
+    expected_R = euler([30, 15, 21])
+    b = expected_R.dot(a)
+    R = find_rigid_rotation(a, b)
+    # in this degenerate case, don't assert that we got the same
+    # rotation, just that it is a valid solution
+    np.testing.assert_array_almost_equal(R.dot(a), b)
+
+
+def test_rigid_rotation_with_reflection():
+    a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v.T
+    b = a * -1
+    R = find_rigid_rotation(a, b)
+    expected_R = np.zeros((3, 3))
+    expected_R[0][0] = expected_R[1][2] = expected_R[2][1] = -1
+    np.testing.assert_array_almost_equal(R, expected_R)
+
+
+def test_rigid_rotation_with_scale():
+    a = Box(origin=np.array([0.0, 0.0, 0.0]), size=np.array([1.0, 1.0, 1.0])).v.T
+    expected_R = euler([30, 15, 21])
+    b = expected_R.dot(a)
+    R = find_rigid_rotation(a, b, allow_scaling=True)
     np.testing.assert_array_almost_equal(R, expected_R)
