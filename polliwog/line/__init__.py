42bad2c8c1184ef959181573f423021ac1483c9b
Paul Melnikow <email@paulmelnikow.com>
2019-12-04 12:10:56 -0500

    Reformat

--- polliwog/line/__init__.py
+++ polliwog/line/__init__.py
@@ -1,5 +1,5 @@
 from . import functions as _functions
 from .functions import *  # noqa: F401,F403
-from .line_intersect import intersect_2d_lines, intersect_lines   # noqa: F401
+from .line_intersect import intersect_2d_lines, intersect_lines  # noqa: F401
 
 __all__ = ["intersect_lines", "intersect_2d_lines"] + _functions.__all__
