9fee49c8c4e2693d7d0b82bc36205463d9b08094
Paul Melnikow <github@paulmelnikow.com>
2019-12-04 12:06:57 -0500

    Attach named coordinate planes to Plane (#145)
    Closes #139

--- polliwog/__init__.py
+++ polliwog/__init__.py
@@ -1,10 +1,9 @@
-__all__ = ["Polyline", "Plane", "Box", "Line", "coordinate_planes", "CompositeTransform",  "CoordinateManager"]
+__all__ = ["Polyline", "Plane", "Box", "Line", "CompositeTransform",  "CoordinateManager"]
 
 from .package_version import __version__
 
 from .box.box import Box
 from .plane.plane import Plane
-from .plane.coordinate_planes import coordinate_planes
 from .line.line import Line
 from .polyline.polyline import Polyline
 from .transform.composite import CompositeTransform
