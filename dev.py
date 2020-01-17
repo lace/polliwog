13b98c25dcb5788cbc5ec8cd3e3fea4492ecf5ce
Paul Melnikow <email@paulmelnikow.com>
2020-01-17 17:43:50 -0500

    Omit tests from wheels

--- dev.py
+++ dev.py
@@ -73,7 +73,7 @@ def clean():
 
 @cli.command()
 def publish():
-    execute("rm -rf dist/")
+    execute("rm -rf dist/ build/")
     execute("python3 setup.py sdist bdist_wheel")
     execute("twine upload dist/*")
 
