1bf39f8cd3e002363742dd6972cf29a2d8fda4e6
Paul Melnikow <github@paulmelnikow.com>
2020-04-22 16:31:19 -0400

    Add `extent()` function (#184)

--- polliwog/pointcloud/_pointcloud_functions.py
+++ polliwog/pointcloud/_pointcloud_functions.py
@@ -28,3 +28,39 @@ def percentile(points, axis, percentile):
     selected_coord_on_axis = np.percentile(coords_on_axis, percentile)
     centroid = np.average(points, axis=0)
     return vg.reject(centroid, axis) + selected_coord_on_axis * axis
+
+
+def extent(points, ret_indices=False):
+    """
+    Find the distance between the two farthest-most points.
+
+    Args:
+        points (np.arraylike): A `kx3` stack of points.
+        ret_indices (bool): When `True`, return the indices along with the
+            distance.
+
+    Returns:
+        object: With `ret_indices=False`, the distance; with
+        `ret_indices=True` a tuple `(distance, first_index, second_index)`.
+
+    Note:
+        This is implemented using a brute-force method.
+    """
+    k = vg.shape.check(locals(), "points", (-1, 3))
+    if k < 2:
+        raise ValueError("At least two points are required")
+
+    farthest_i = -1
+    farthest_j = -1
+    farthest_distance = -1
+    for i, probe in enumerate(points):
+        distances = vg.euclidean_distance(points, probe)
+        this_farthest_j = np.argmax(distances)
+        if distances[this_farthest_j] > farthest_distance:
+            farthest_i = i
+            farthest_j = this_farthest_j
+            farthest_distance = distances[this_farthest_j]
+    if ret_indices:
+        return farthest_distance, farthest_i, farthest_j
+    else:
+        return farthest_distance
