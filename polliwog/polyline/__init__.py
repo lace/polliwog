afc77d766de8a058eceb7817d381c051c6a4f1be
Paul Melnikow <email@paulmelnikow.com>
2022-04-08 17:13:36 -0400

    Fixes

--- polliwog/polyline/__init__.py
+++ polliwog/polyline/__init__.py
@@ -1,4 +1,4 @@
-from ._inflection_points import inflection_points, point_of_max_acceleration
 from ._edges import edges_for
+from ._inflection_points import inflection_points, point_of_max_acceleration
 
 __all__ = ["edges_for", "inflection_points", "point_of_max_acceleration"]
--- polliwog/polyline/_edges.py
+++ polliwog/polyline/_edges.py
@@ -4,6 +4,16 @@ EDGE_DTYPE = np.int64
 
 
 def edges_for(num_v, is_closed):
+    """
+    Compute edges of a polyline, as an array of vertex indices for each edge.
+
+    Args:
+        num_v (int): The number of vertices.
+        is_closed (bool): Whether or not the polyline is closed.
+
+    Returns:
+        np.ndarray: The `kx2` edges array.
+    """
     num_e = num_v if is_closed else num_v - 1
 
     if num_e == 0:
