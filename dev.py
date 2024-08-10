b29fb96f158dd2487c4c8fdadb7dd38862983e38
Paul Melnikow <email@paulmelnikow.com>
2024-08-10 10:56:33 -0400

    Update CLI option

--- dev.py
+++ dev.py
@@ -20,7 +20,7 @@ def cli():
 
 @cli.command()
 def install():
-    execute("poetry install --remove-untracked --extras serialization")
+    execute("poetry install --sync --extras serialization")
 
 
 @cli.command()
